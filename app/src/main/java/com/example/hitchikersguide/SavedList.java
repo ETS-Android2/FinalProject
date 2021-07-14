2464919cc60b73c31f85a1b5691da02c3b52bdaa
Jenne <jkmiklos@hotmail.com>
2021-07-14 16:11:38 -0400

    updates to layouts, move through activities

--- app/src/main/java/com/example/hitchikersguide/SavedList.java
+++ app/src/main/java/com/example/hitchikersguide/SavedList.java
@@ -1,8 +1,11 @@
 package com.example.hitchikersguide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SavedList extends AppCompatActivity {
 
@@ -11,7 +14,16 @@ public class SavedList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_list);
 
+        //Define Next Activity
+        Intent activityJK = new Intent(this, Jokes.class);
+
+        Button jokeButton = findViewById(R.id.SL_ToJokesButton);
+        jokeButton.setOnClickListener(click  -> startActivity(activityJK));
+
         // Listview
+        RecyclerView imgList = findViewById(R.id.SL_ListOfImages);
+        //imgList.layoutM
+        //imgList.setAdapter(ListAdapter myListAdapter);
         // Progress bar will be on async task
 
 

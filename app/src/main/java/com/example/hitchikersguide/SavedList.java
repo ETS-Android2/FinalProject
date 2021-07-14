4ac0f5765f746e01571cfe180b943ebffbad0a4c
Jenne <jkmiklos@hotmail.com>
2021-07-14 17:05:04 -0400

    started list view and SpacePic class to store image data

--- app/src/main/java/com/example/hitchikersguide/SavedList.java
+++ app/src/main/java/com/example/hitchikersguide/SavedList.java
@@ -1,11 +1,18 @@
 package com.example.hitchikersguide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class SavedList extends AppCompatActivity {
 
@@ -18,14 +25,50 @@ public class SavedList extends AppCompatActivity {
         Intent activityJK = new Intent(this, Jokes.class);
 
         Button jokeButton = findViewById(R.id.SL_ToJokesButton);
-        jokeButton.setOnClickListener(click  -> startActivity(activityJK));
+        jokeButton.setOnClickListener(click -> startActivity(activityJK));
 
         // Listview
-        RecyclerView imgList = findViewById(R.id.SL_ListOfImages);
-        //imgList.layoutM
-        //imgList.setAdapter(ListAdapter myListAdapter);
+        ListView imgList = findViewById(R.id.SL_ListOfImages);
+        MyAdapter myAdapter = new MyAdapter();
+        imgList.setAdapter(myAdapter);
+
         // Progress bar will be on async task
+    }
 
+    /**
+     * Adapter Class for ListView
+     */
+    private class MyAdapter extends BaseAdapter {
 
+        @Override
+        public int getCount() {
+            return 0;//elements.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;//elements.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return (long) position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View newView = convertView;
+            LayoutInflater inflater = getLayoutInflater();
+
+//            // make a new row
+//            newView = inflater.inflate(elements.get(position).getLayout(), parent, false);
+//
+//            //set text for new row
+//            TextView tView = newView.findViewById(elements.get(position).getTextId());
+//            tView.setText(elements.get(position).msgText);
+
+            // return new row to be added to table
+            return newView;
+        }
     }
 }
---
+++ app/src/main/java/com/example/hitchikersguide/SpacePic.java
@@ -0,0 +1,13 @@
+package com.example.hitchikersguide;
+
+public class SpacePic {
+    String imgDate;
+    String imgTitle;
+    String imgDescroiption;
+    String imgURL;
+    String imgHDURL;
+
+    public SpacePic(){
+        // send in parameters from JSON and save them.
+    }
+}

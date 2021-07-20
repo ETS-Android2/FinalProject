b97a1b6e5ffc74fff9ca53162240d9b96bd4b499
blgue <guer0183@algonquinlive.com>
2021-07-19 20:36:59 -0400

    Added Jokes:Toast and ImageDisplay:Progress Bar

--- app/src/main/java/com/example/hitchikersguide/Jokes.java
+++ app/src/main/java/com/example/hitchikersguide/Jokes.java
@@ -3,13 +3,25 @@ package com.example.hitchikersguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Jokes extends AppCompatActivity {
+    TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jokes);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(click -> {
+                    Toast.makeText(this, "This will display the answer to a joke.", Toast.LENGTH_LONG).show();
+                });
 
         //Toast with the answer when you hit the button
     }

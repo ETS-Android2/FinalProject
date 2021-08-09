541f4d60f77fdbb723458d541277898e2fe82c39
Jenne <64040887+JediStamp@users.noreply.github.com>
2021-08-09 13:34:02 -0400

    Revert "Jenne 09082021"

--- app/src/main/java/com/example/hitchikersguide/Jokes.java
+++ app/src/main/java/com/example/hitchikersguide/Jokes.java
@@ -1,75 +1,31 @@
 package com.example.hitchikersguide;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.TextView;
-import com.google.android.material.snackbar.Snackbar;
-import java.util.Random;
+import android.widget.Toast;
 
-/**
- * Jokes page displays selected space jokes. Cause we got jokes son.
- * Extends Base Activity to provide toolbar and nav drawer
- *
- * @author Brianna Guerin
- * @author Jenne Stamplecoskie
- */
-public class Jokes extends BaseActivity {
-    TextView JK_show_joke;
-    Button JK_get_joke;
-    Button JK_answer;
+public class Jokes extends AppCompatActivity {
+    TextView textView;
+    Button button;
 
-    /**
-     * On Create Function initializes widgets and listeners
-     *
-     * @param savedInstanceState - the state that the app was last saved in
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes);
 
-        // Inflate the jokes page layout into the Base activity frame
-        FrameLayout contentFrameLayout = findViewById(R.id.content_frame);
-        getLayoutInflater().inflate(R.layout.activity_jokes, contentFrameLayout);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
 
-        // Initialize page elements
-        JK_show_joke = findViewById(R.id.JK_textView);
-        JK_get_joke = findViewById(R.id.JK_getjoke);
-        JK_answer = findViewById(R.id.JK_answer);
+        button.setOnClickListener(click -> {
+                    Toast.makeText(this, "This will display the answer to a joke.", Toast.LENGTH_LONG).show();
+                });
 
-        // Our Jokes
-        String[][] jokes = {
-                {"I am throwing a party in space", "Can you help me planet?"},
-                {"Where do the keyboards go to have dinner?", "The space bar."},
-                {"Why did the cow go to outer space?", "To visit the milky way."},
-                {"Why don't aliens eat clowns?", "They taste funny!"},
-                {"What kind of music do planets sing?", "Nep-tunes!"},
-                {"Saturn's name is the best in our solar system", "It has a nice ring to it."},
-                {"I'm reading a book about anti-gravity.", "It's hard to put down."},
-                {"Yesterday I was charged $100,000 for sending my cat into space.", "It was a cat astro fee."},
-                {"Why did the Americans win the space race?", "Because the Soviets were Stalin"},
-                {"How did the space teddy cross the road?", "Ewoked."},
-                {"Why did the star go to school?", "So it could get brighter"},
-                {"I'm addicted to space jokes.", "Someday I'll over-comet"},
-                {"My kid is obsessed with the moon", "I'm hoping it's just a phase."},
-                {"What's E.T. short for?", "He has little legs."},
-                {"Why didn't the sun go to college?", "It already has a million degrees."},
-                {"How does the man on the moon cut his hair?", "Eclipse it."},
-                {"What does the astronaut use to keep his feet warm?", "A space heater."},
-                {"How do you start a fight in outer space?", "Comet me bro!"}
-        };
+        //Toast with the answer when you hit the button
 
-        // Get random joke
-        JK_get_joke.setOnClickListener(click -> {
-            Random rand = new Random();
-            int max = jokes.length;
-            int num = rand.nextInt(max);
+        //TODO: Toast with the answer when you hit the button
 
-            JK_show_joke.setText(jokes[num][0]);
-            String answer = jokes[num][1];
-
-            // Get joke answer
-            JK_answer.setOnClickListener(v -> Snackbar.make(JK_show_joke, answer, Snackbar.LENGTH_LONG).show());
-        });
     }
 }
--- app/src/main/java/com/example/hitchikersguide/SpacePic.java
+++ app/src/main/java/com/example/hitchikersguide/SpacePic.java
@@ -1,12 +1,5 @@
 package com.example.hitchikersguide;
 
-import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-
-import androidx.annotation.NonNull;
-
 /**
  * Class to define the Space Picture Object
  *
@@ -14,55 +7,26 @@ import androidx.annotation.NonNull;
  * @author Jenne Stamplecoskie
  */
 public class SpacePic {
-    long   imgID;
     String imgDate;
     String imgURL;
+    long imgID;
     String imgTitle;
-    String imgDetails;
+    String imgDescription;
     String imgHDURL;
 
-    /**
-     * Space Pic class holds the saved images of the day and their associated data.
-     *
-     * @param imgDate - the date of the image
-     * @param imgURL - url to the image
-     * @param imgID - location in the pictures array
-     */
-    public SpacePic(long imgID, String imgDate, String imgURL){
+
+    public SpacePic(String imgDate, String imgURL, long imgID){
         // send in parameters from JSON and save them.
         this.imgDate = imgDate;
         this.imgURL = imgURL;
         this.imgID = imgID;
     }
 
-    public SpacePic(String imgDate, String imgTitle, String imgURL, String imgHDURL, String imgDetails) {
-        this.imgDate = imgDate;
-        this.imgURL = imgURL;
-        this.imgTitle = imgTitle;
-        this.imgDetails = imgDetails;
-        this.imgHDURL = imgHDURL;
-    }
-
-    public void setTitle(String title){
-
-        this.imgTitle = title;
-    }
-
-    public void setDetails(String details){
-
-        this.imgDetails = details;
-    }
-
-    public void setHDURL(String hdurl){
-
-        this.imgHDURL = hdurl;
-    }
-
-    public long getImgID() {
-        return imgID;
-    }
     // TODO: save title for the image
     // TODO: open HDURL in web browser
-    // TODO: DO we make variables private and use getter methods? Probably should
-
+    //Mod 3 Intents
+//    String url = "http://www.algonquincollege.com";
+//    Intent i = new Intent(Intent.ACTION_VIEW);
+//    i.setData( Uri.parse(url) );
+//    startActivity(i);
 }

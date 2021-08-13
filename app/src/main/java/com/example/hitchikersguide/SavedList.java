56e2335749b93fd7896de6904e07674aa86396d1
Jenne <64040887+JediStamp@users.noreply.github.com>
2021-08-12 22:46:11 -0400

    Revert "Milestone 4"

--- app/src/main/java/com/example/hitchikersguide/SavedList.java
+++ app/src/main/java/com/example/hitchikersguide/SavedList.java
@@ -1,34 +1,30 @@
 package com.example.hitchikersguide;
 
 import androidx.appcompat.app.AlertDialog;
-import android.content.ContentValues;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.FrameLayout;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
 
 /**
  * Save List Activity holds the list of saved images
- * Includes fragment to view item details
- * Allows user to remove from list and database
+ * Links to joke page through Jokes Button
  *
  * @author Brianna Guerin
  * @author Jenne Stamplecoskie
  */
-public class SavedList extends BaseActivity {
-    private ArrayList<SpacePic> pictures = new ArrayList<>();
-    private SQLiteDatabase myDB;
-    String imgDate, imgTitle, imgURL, imgDetails, imgHDURL;
-    SpacePic pic;
-    static boolean isTablet;
+public class SavedList extends AppCompatActivity {
+    private ArrayList<SpacePic> pictures = new ArrayList<>(); // Messages
 
     /**
      * On Create Function initializes widgets and listeners
@@ -38,135 +34,64 @@ public class SavedList extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_list);
 
-        // Inflate the Main Activity layout into the Base activity frame
-        FrameLayout contentFrameLayout = findViewById(R.id.content_frame);
-        getLayoutInflater().inflate(R.layout.activity_saved_list, contentFrameLayout);
+        // Define Next Activity - Jokes
+        Intent activityJK = new Intent(this, Jokes.class);
 
-        isTablet = findViewById(R.id.fragmentLocation) != null;
+        // Button to take you to Joke activity
+        Button jokeButton = findViewById(R.id.SL_ToJokesButton);
+        jokeButton.setOnClickListener(click -> startActivity(activityJK));
 
+        // Listview
         ListView imgList = findViewById(R.id.SL_ListOfImages);
         MyAdapter myAdapter = new MyAdapter();
         imgList.setAdapter(myAdapter);
-        myAdapter.notifyDataSetChanged();
-        loadSavedPics();
 
-        Intent passImg = getIntent();
-        imgTitle = passImg.getStringExtra("Title");
-        imgDate = passImg.getStringExtra("Date");
-        imgURL = passImg.getStringExtra("URL");
-        imgHDURL = passImg.getStringExtra("HDURL");
-        imgDetails = passImg.getStringExtra("Details");
-
-        if (imgTitle != null) {
-            ContentValues newRowValues = new ContentValues();
-            newRowValues.put(MyDBOpener.COL_DATE, imgDate);
-            newRowValues.put(MyDBOpener.COL_URL, imgURL);
-            newRowValues.put(MyDBOpener.COL_HDURL, imgHDURL);
-            newRowValues.put(MyDBOpener.COL_TITLE, imgTitle);
-            newRowValues.put(MyDBOpener.COL_DETAIL, imgDetails);
-            long newID = myDB.insert(MyDBOpener.TABLE_NAME, null, newRowValues);
-
-            pic = new SpacePic(imgDate, imgTitle, imgURL, imgHDURL, imgDetails);
+        //Add elements to the list view
+        // TODO: Remove once we have a proper list of saved images
+        SpacePic pic;
+        for ( int i = 1; i <= 8; i++ ){
+            pic = new SpacePic("date" + i, "url" + i, i);
             pictures.add(pic);
-            myAdapter.notifyDataSetChanged();
         }
 
         // Get details of an item on the list
         imgList.setOnItemClickListener((parent, view, position, id) -> {
-
-            Bundle dataToPass = new Bundle();
-            dataToPass.putString("Date", pictures.get(position).imgDate);
-            dataToPass.putString("Title", pictures.get(position).imgTitle);
-            dataToPass.putString("URL", pictures.get(position).imgURL);
-            dataToPass.putString("HDURL", pictures.get(position).imgHDURL);
-            dataToPass.putString("Details", pictures.get(position).imgDetails);
-
-            if (isTablet) {
-                DetailsFragment dFragment = new DetailsFragment();
-                dFragment.setArguments(dataToPass);
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLocation, dFragment).commit();
-            }
-            else {
-                Intent activityEM = new Intent(this, EmptyActivity.class);
-                activityEM.putExtras(dataToPass);
-                startActivity(activityEM);
-            }
+            String details = "image date: " + pictures.get(position).imgDate +
+                    " url: " + pictures.get(position).imgURL + " ID: " +
+                    pictures.get(position).imgID;
+            Snackbar.make(imgList, details, Snackbar.LENGTH_LONG).show();
         });
 
         // Remove an item from the list
         imgList.setOnItemLongClickListener(
-            // Create a Dialog
-            (parent, view, position, id) -> {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-                alertDialogBuilder.setTitle(getResources().getString(R.string.SL_Alert_Title))
+                // Create a Dialog
+                (parent, view, position, id) -> {
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                    alertDialogBuilder.setTitle(getResources().getString(R.string.SL_Alert_Title))
 
-                    // Message
-                    .setMessage(getResources().getString(R.string.SL_Alert_msg1) + position + "\n"
-                            + getResources().getString(R.string.SL_Alert_msg2) + id)
+                            // Message
+                            .setMessage(getResources().getString(R.string.SL_Alert_msg1) + position + "\n"
+                                    + getResources().getString(R.string.SL_Alert_msg2) + id)
 
-                    // Yes Action
-                    .setPositiveButton(R.string.yes, (click, arg) -> {
-                        pic = pictures.get(position);
-                        pictures.remove(position);
-                        deleteSpacePic(pic);
-                        myAdapter.notifyDataSetChanged();
-                        if (isTablet) {
-                            this.getSupportFragmentManager().beginTransaction()
-                                    .remove(getSupportFragmentManager().findFragmentById(R.id.fragmentLocation))
-                                    .commit();
-                        }
-                    })
+                            // Yes Action
+                            .setPositiveButton(R.string.yes, (click, arg) -> {
+                                pictures.remove(position);
+                                myAdapter.notifyDataSetChanged();
+                            })
 
-                    // No action
-                    .setNegativeButton(R.string.no, (click, arg) -> { })
+                            // No action
+                            .setNegativeButton(R.string.no, (click, arg) -> { })
 
-                    //Show the dialog
-                    .create().show();
-                return true;
-            } );
-    }
+                            //Show the dialog
+                            .create().show();
+                    return true;
+                } );
 
-    /**
-     * Loads the pictures saved in the database into the image list
-     */
-    private void loadSavedPics() {
-        Cursor results;
-        SpacePic curPic;
-        // Connect to DB
-        MyDBOpener dbOpen = new MyDBOpener(this);
-        myDB = dbOpen.getWritableDatabase();
-
-        // list of columns
-        String[] columns = {MyDBOpener.COL_ID, MyDBOpener.COL_DATE, MyDBOpener.COL_URL,
-                            MyDBOpener.COL_HDURL, MyDBOpener.COL_TITLE, MyDBOpener.COL_DETAIL};
-        // get all entries
-        results = myDB.query(false, MyDBOpener.TABLE_NAME, columns, null,
-                null, null, null, null, null);
-
-        // Get column indices
-        int idColIdx = results.getColumnIndex(MyDBOpener.COL_ID);
-        int dateColIdx = results.getColumnIndex(MyDBOpener.COL_DATE);
-        int urlColIdx = results.getColumnIndex(MyDBOpener.COL_URL);
-        int hdurlColIdx = results.getColumnIndex(MyDBOpener.COL_HDURL);
-        int titleColIdx = results.getColumnIndex(MyDBOpener.COL_TITLE);
-        int detailColIdx = results.getColumnIndex(MyDBOpener.COL_DETAIL);
-
-
-        // Iterate over the results, return true if there is a next item:
-        while (results.moveToNext()) {
-            // Create an image and add it to the arrayList
-            curPic = new SpacePic(results.getLong(idColIdx), results.getString(dateColIdx),
-                    results.getString(urlColIdx));
-            curPic.setHDURL(results.getString(hdurlColIdx));
-            curPic.setTitle(results.getString(titleColIdx));
-            curPic.setDetails(results.getString(detailColIdx));
-            pictures.add(curPic);
-        }
-        results.close();
-    }
-    protected void deleteSpacePic(SpacePic pic) {
-        myDB.delete(MyDBOpener.TABLE_NAME, MyDBOpener.COL_ID + "= ?", new String[] {Long.toString(pic.getImgID())});
+        //TODO: Progress bar will be on async task
+        //TODO: Snackbar to give list details
+        //TODO: Onclick listener
     }
 
     /**
@@ -181,33 +106,30 @@ public class SavedList extends BaseActivity {
 
         @Override
         public Object getItem(int position) {
-
             return pictures.get(position);
         }
 
         @Override
         public long getItemId(int position) {
-
             return position;
         }
 
         @Override
         public View getView(int position, View myView, ViewGroup parent) {
+            //View newView = convertView;
             LayoutInflater inflater = getLayoutInflater();
 
             // make a new row
             myView = inflater.inflate(R.layout.img_list_row, parent, false);
+            // TODO: Add view holder pattern in
 
             //set text for new row
-            TextView dateView = myView.findViewById(R.id.IL_Date);
+            TextView dateView = myView.findViewById(R.id.DateGoesHere);
             dateView.setText(pictures.get(position).imgDate);
-
-            TextView urlView = myView.findViewById(R.id.IL_URL);
+            TextView urlView = myView.findViewById(R.id.TextGoesHere);
             urlView.setText(pictures.get(position).imgURL);
 
-            TextView titleView = myView.findViewById(R.id.IL_Title);
-            titleView.setText(pictures.get(position).imgTitle);
-
+            // return new row to be added to table
             return myView;
         }
     }
--- app/src/main/java/com/example/hitchikersguide/SpacePic.java
+++ app/src/main/java/com/example/hitchikersguide/SpacePic.java
@@ -7,81 +7,26 @@ package com.example.hitchikersguide;
  * @author Jenne Stamplecoskie
  */
 public class SpacePic {
-    long   imgID;
     String imgDate;
     String imgURL;
+    long imgID;
     String imgTitle;
-    String imgDetails;
+    String imgDescription;
     String imgHDURL;
 
-    /**
-     * Space Pic class holds the saved images of the day and their associated data.
-     *
-     * @param imgDate - the date of the image
-     * @param imgURL - url to the image
-     * @param imgID - location in the pictures array
-     */
-    public SpacePic(long imgID, String imgDate, String imgURL){
+
+    public SpacePic(String imgDate, String imgURL, long imgID){
         // send in parameters from JSON and save them.
         this.imgDate = imgDate;
         this.imgURL = imgURL;
         this.imgID = imgID;
     }
 
-    /**
-     *  Space Pic class holds the saved images of the day and their associated data.
-     *
-     * @param imgDate - the date of the image
-     * @param imgTitle - the title of the image
-     * @param imgURL - url to the image
-     * @param imgHDURL - high definition url to the image
-     * @param imgDetails - image description
-     */
-    public SpacePic(String imgDate, String imgTitle, String imgURL, String imgHDURL, String imgDetails) {
-        this.imgDate = imgDate;
-        this.imgURL = imgURL;
-        this.imgTitle = imgTitle;
-        this.imgDetails = imgDetails;
-        this.imgHDURL = imgHDURL;
-    }
-
-    /**
-     * Set the Title of the SpacePic
-     *
-     * @param title - Title to be set for SpacePic
-     */
-    public void setTitle(String title){
-
-        this.imgTitle = title;
-    }
-
-    /**
-     * Set the Description of the SpacePic
-     *
-     * @param details - Description to be set for SpacePic
-     */
-    public void setDetails(String details){
-
-        this.imgDetails = details;
-    }
-
-    /**
-     * Set the HD URL of the SpacePic
-     *
-     * @param hdurl - high definition URL to be set for SpacePic
-     */
-    public void setHDURL(String hdurl){
-
-        this.imgHDURL = hdurl;
-    }
-
-    /**
-     * Get the Image ID
-     *
-     * @return - ImageID is returned
-     */
-    public long getImgID() {
-
-        return imgID;
-    }
+    // TODO: save title for the image
+    // TODO: open HDURL in web browser
+    //Mod 3 Intents
+//    String url = "http://www.algonquincollege.com";
+//    Intent i = new Intent(Intent.ACTION_VIEW);
+//    i.setData( Uri.parse(url) );
+//    startActivity(i);
 }
